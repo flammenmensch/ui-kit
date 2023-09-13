58804a6746f57dab82a67e9b5488dab7975e1ac8
Flammenmensch <maleventum@gmail.com>
2023-09-13 01:23:16 +0100

    Tweak overlay color

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -23,7 +23,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       'isolate text-sm group rounded-full shrink-0 px-4 py-2 transition-colors transition-shadows overflow-hidden relative outline-offset-2 outline-accent',
       {
         'bg-foreground text-background': variant === 'primary',
-        'bg-slate-100 text-foreground': variant === 'secondary',
+        'bg-stone-100 text-foreground': variant === 'secondary',
         'bg-accent text-background': variant === 'accent',
         'bg-danger text-white': variant === 'danger',
         'bg-success text-white': variant === 'success',
@@ -32,7 +32,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 
     return (
       <RACButton {...rest} className={cn} ref={ref}>
-        <span className="transition-all absolute inset-0 opacity-0 -z-10 group-data-[hovered]:opacity-20 group-data-[pressed]:opacity-40 bg-stone-500 rounded-none" />
+        <span className="transition-all absolute inset-0 opacity-0 -z-10 group-data-[hovered]:opacity-20 group-data-[pressed]:opacity-40 bg-stone-400 rounded-none" />
         {children}
       </RACButton>
     );
